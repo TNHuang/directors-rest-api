f1cb0036f10b7b5b16c33958481da7e1d6d9ee67
Kenny Huang <KHuangtai@gmail.com>
2014-12-31 22:48:32 -0500

    server finished, start testing

--- server.js
+++ server.js
@@ -4,3 +4,19 @@ var express = require('express'),
 	bodyParser = require('body-parser'),
 	mongoose = require('mongoose'),
 	router = require('./routers');
+
+var port = process.env.PORT || 8080;
+
+//enable post request processor
+app.use( bodyParser.urlencoded({ extended: true }) );
+app.user(bodyParser.json());
+
+//registered all routes with /api prefix
+app.use("/api", router);
+
+//listen to port via http, since express 4.0 don't bunlde http anymore
+http.createServer(app).listen(port);
+console.log("api listening on port ", port);
+
+//connect to localhost mongoDB server
+moongoose.connect('mongodb://127.0.0.1:27017/myDirectors');
